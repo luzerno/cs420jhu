3a2011e1c73b2d67e9d7f20be1e768ba109e5091
luzerno <yifan.ge@outlook.com>
2013-02-20 19:28:55 +0000

    hello

--- ompsmooth/run.sh
+++ ompsmooth/run.sh
@@ -2,25 +2,25 @@ rm result
 echo "run 1"
 echo "run 1" >> result
 ./smooth >> result
-echo "========================================="> result
+echo "=========================================">> result
 
 echo "run 2"
 echo "run 2" >> result
 ./smooth >> result
-echo "========================================="> result
+echo "=========================================">> result
 
 
 echo "run 3"
 echo "run 3" >> result
 ./smooth >> result
-echo "========================================="> result
+echo "=========================================">> result
 
 echo "run 4"
 echo "run 4" >> result
 ./smooth >> result
-echo "========================================="> result
+echo "=========================================">> result
 
 echo "run 5"
 echo "run 5" >> result
 ./smooth >> result
-echo "========================================="> result
+echo "=========================================">> result
