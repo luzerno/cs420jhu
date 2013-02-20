2fb00f8f4e0a909bd5b640cb75130ef11e022723
Ubuntu <ubuntu@ip-10-108-162-62.ec2.internal>
2013-02-20 18:23:42 +0000

    script

---
+++ ompsmooth/run.sh
@@ -0,0 +1,20 @@
+echo "run 1" >> result
+./smooth >> result
+echo "========================================="> result
+
+echo "run 2" >> result
+./smooth >> result
+echo "========================================="> result
+
+
+echo "run 3" >> result
+./smooth >> result
+echo "========================================="> result
+
+echo "run 4" >> result
+./smooth >> result
+echo "========================================="> result
+
+echo "run 5" >> result
+./smooth >> result
+echo "========================================="> result
